99ccb777c7b0b89b3b1c84af0c74777319ad7b43
Krishnkant <krishnkantpachori50@gmail.com>
2022-02-27 13:54:26 +0530

    this is 2nd commit

--- client/src/context/TransactionContext.jsx
+++ client/src/context/TransactionContext.jsx
@@ -1,7 +1,7 @@
 import React,{useState,useEffect} from "react";
 import {ethers} from 'ethers';
 
-import {contractABI,contractAddress} from '../utils/Transactions.json';
+import {contractABI,contractAddress} from '../utils/constants';
 
 export const TransactionContext=React.createContext();
 
@@ -15,6 +15,12 @@ const getEthereumContract=()=>{
 
 export const TransactionProvider=({children})=>{
     const [currentAccount, setcurrentAccount] = useState('');
+    const [formData, setformData] = useState({addressTo:'',amount:'',keyword:'',message:''});
+
+    const handleChange=(e,name)=>{
+      setformData((prevState)=>({...prevState,[name]:e.target.value}));
+    }
+
     const checkIfWalletIsConnected=async ()=>{
         try {
             if(!ethereum) return alert("please install metamask");
@@ -48,12 +54,25 @@ export const TransactionProvider=({children})=>{
         }
     }
 
+    const sendTransaction=()=>{
+        try {
+            if(!ethereum) return alert("please install metamask");
+
+            // get the data from the form....
+            const { addressTo, amount, keyword, message } = formData;
+
+            getEthereumContract();
+        } catch (error) {
+            throw new Error("NO ethereum object");
+        }
+    }
+
     useEffect(() => {
      checkIfWalletIsConnected();
     }, []);
     
 return (
-    <TransactionContext.Provider value={{connectWallet,currentAccount}}>
+    <TransactionContext.Provider value={{connectWallet,currentAccount,formData,setformData,handleChange,sendTransaction}}>
         {children}
     </TransactionContext.Provider>
 )
